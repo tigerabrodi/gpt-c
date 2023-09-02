fdc6e165a33dda9279ce02f51fa0a49869f26a88
Tiger Abrodi <tigerabrodi@gmail.com>
2023-09-02 15:40:31 +0200

    commit

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// chatgpt clone based on gpt-3.5
 int main()
 {
-
+	char *initial_message = "Hello, how can I help you today?";
+	char initial_message_in_array[100];
 	return 0;
 }
